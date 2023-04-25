62f0851560c96d8501b66a6512d0e407859c5ee8
Steven Stackhouse <stackmm@gmail.com>
2023-04-25 10:36:09 -0600

    add add_biker tests

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe BikeClub do
     expect(club.name).to eq("The Squad")
     expect(club.bikers).to eq([])
   end
+
+  it "can add bikers to the club" do
+    club = BikeClub.new("The Squad")
+    biker1 = Biker.new("Kenny", 30)
+    biker2 = Biker.new("Athena", 15)
+
+    expect(club.bikers).to eq([])
+    club.add_biker(biker1)
+    expect(club.bikers).to eq([biker1])
+    club.add_biker(biker2)
+    expect(club.bikers).to eq([biker1, biker2])
+  end
 end
