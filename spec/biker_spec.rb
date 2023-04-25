2b6c32be321b86373558c73beac43c2f07e6ca3f
Steven Stackhouse <stackmm@gmail.com>
2023-04-25 09:22:55 -0600

    add initialize tests

---
+++ spec/biker_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/ride'
+require './lib/biker'
+
+RSpec.describe Biker do
+  it "exists" do
+    biker = Biker.new("Kenny", 30)
+    expect(biker).to be_an_instance_of(Biker)
+  end
+
+  it "has attributes" do
+    biker = Biker.new("Kenny", 30)
+    expect(biker.name).to eq("Kenny")
+    expect(biker.max_distance).to eq(30)
+    expect(biker.rides).to eq({})
+    expect(biker.acceptable_terrain).to eq([])
+  end
+end
