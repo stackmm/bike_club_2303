cf0f078987a2a24dd3d6084eb0946969f50924af
Steven Stackhouse <stackmm@gmail.com>
2023-04-25 09:08:49 -0600

    add initialize

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,10 @@
 class Ride
-
+  attr_reader :name, :distance, :loop, :terrain
+  
+  def initialize(attributes)
+    @name = attributes[:name]
+    @distance = attributes[:distance]
+    @loop = attributes[:loop]
+    @terrain = attributes[:terrain]
+  end
 end
