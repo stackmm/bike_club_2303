3f8c98070a448cf3c04b6205f7ba83a1ac4d1068
Steven Stackhouse <stackmm@gmail.com>
2023-04-25 09:12:59 -0600

    add total_distance method

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,10 +1,18 @@
 class Ride
   attr_reader :name, :distance, :loop, :terrain
-  
+
   def initialize(attributes)
     @name = attributes[:name]
     @distance = attributes[:distance]
     @loop = attributes[:loop]
     @terrain = attributes[:terrain]
+  end
+
+  def total_distance
+    if @loop
+      @distance
+    else
+      @distance * 2
+    end
   end
 end
