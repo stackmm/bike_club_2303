a7ad398533fc26fd92ed0b477d5e6d740bca23b1
Steven Stackhouse <stackmm@gmail.com>
2023-04-25 10:35:54 -0600

    add add_biker method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -5,5 +5,10 @@ class BikeClub
     @name = name
     @bikers = []
   end
-  
+
+  def add_biker(biker)
+    @bikers << biker
+    @bikers # asked for a return value of an Array of Biker Objects
+  end
+
 end
