a96d1ffa49f99cdc26349d4ceadb0c8d9065dcd5
Steven Stackhouse <stackmm@gmail.com>
2023-04-25 11:13:53 -0600

    add best_time method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -8,12 +8,24 @@ class BikeClub
 
   def add_biker(biker)
     @bikers << biker
-    @bikers # asked for a return value of an Array of Biker Objects
+    @bikers # the method asked for a return value of an Array of Biker Objects
   end
 
   def most_rides
     @bikers.max_by do |biker|
       biker.rides.values.flatten.length
+    end
+  end
+
+  def best_time(ride)
+    @bikers.min_by do |biker|
+      if biker.rides.keys.include?(ride)
+        biker.personal_record(ride)
+      else  
+        # if a biker's @rides does NOT contain that specific Ride, 
+        # use inifinity for the comparison so the biker with a time logged will always be less
+        Float::INFINITY
+      end
     end
   end
 
